08d2a35e2aee2393e2b63656d84a10962b45c60f
Anand Chaudhary <anand92490@gmail.com>
2019-10-25 13:27:19 -0500

    root index.js configured and app.js setup

--- react-click-game/src/App.js
+++ react-click-game/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import MatchCard from "./components/MatchCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+// import matches from "./matchcards.json.js";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {  };
 
+
+  render() { 
+    return ( 
+      <Wrapper>
+        <Title> Clicky Game!</Title>
+      <MatchCard />
+      </Wrapper>
+    
+     );
+  }
+}
+ 
 export default App;
